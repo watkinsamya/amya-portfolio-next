1b5a64ac1bc8071c6ed5a36806f5a2dee1db6e0d
Amya Watkins <watkinsamya03@gmail.com>
2025-08-28 21:40:27 -0400

    chore: stop tracking .next/out and respect .gitignore

--- .next/static/chunks/app_page_tsx_70e1697c._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_d02d8fbc._.js",
-  "static/chunks/_5acd343e._.js"
-],
-    source: "dynamic"
-});
--- .next/static/chunks/app_projects_page_tsx_70e1697c._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_74919255._.js",
-  "static/chunks/_3cf517d7._.js"
-],
-    source: "dynamic"
-});
